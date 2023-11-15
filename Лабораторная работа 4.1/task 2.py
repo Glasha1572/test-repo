788ee252df67362f64304e5b2dd6a8432662129e
Glasha1572 <148009414+Glasha1572@users.noreply.github.com>
2023-11-15 23:47:28 +0300

    Add files via upload

---
+++ Лабораторная работа 4.1/task 2.py
@@ -0,0 +1,10 @@
+ # TODO импортировать необходимые молули
+
+import csv
+import json
+
+src = input("Введите путь до файла CSV: ")
+
+def konverter(src):
+    lines = [line for line in csv.DictReader(src)]
+    print(json.dumps([lines], 4))
